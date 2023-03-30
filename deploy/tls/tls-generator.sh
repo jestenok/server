f1f52bea6a015213846784abd62c7410befb8f45
jestenok <112510943+jestenok@users.noreply.github.com>
2023-03-30 18:03:38 +0800

    add ssl for jira

--- deploy/tls/tls-generator.sh
+++ deploy/tls/tls-generator.sh
@@ -5,22 +5,31 @@ chmod +x /usr/local/bin/cfssl && \
 chmod +x /usr/local/bin/cfssljson
 
 #generate ca in /tmp
-cfssl gencert -initca ca-csr.json | cfssljson -bare /tmp/ca
+cfssl gencert -initca ca-csr.json | cfssljson -bare /code/deploy/tls/certs/jira/ca
+
+##generate certificate in /tmp
+#cfssl gencert \
+#  -ca=/code/deploy/tls/certs/vault/ca.pem \
+#  -ca-key=/code/deploy/tls/certs/vault/ca-key.pem \
+#  -config=ca-config.json \
+#  -hostname="vault,vault.vault.svc.cluster.local,vault.vault.svc,localhost,127.0.0.1,vault.jestenok.com" \
+#  -profile=default \
+#  ca-csr.json | cfssljson -bare /code/deploy/tls/certs/jira
 
 #generate certificate in /tmp
 cfssl gencert \
-  -ca=/tmp/ca.pem \
-  -ca-key=/tmp/ca-key.pem \
+  -ca=/code/deploy/tls/certs/jira/ca.pem \
+  -ca-key=/code/deploy/tls/certs/jira/ca-key.pem \
   -config=ca-config.json \
-  -hostname="vault,vault.vault.svc.cluster.local,vault.vault.svc,localhost,127.0.0.1,jestenok.com" \
+  -hostname="jira,jira.jira.svc.cluster.jira,jira.jira.svc,localhost,127.0.0.1,jira.jestenok.com" \
   -profile=default \
-  ca-csr.json | cfssljson -bare /tmp/vault
+  ca-csr.json | cfssljson -bare /code/deploy/tls/certs/jira/
 
-#creating secrets
-kubectl -n consul create secret tls tls-ca \
- --cert deploy/tls/certs/ca.pem  \
- --key deploy/tls/certs/ca-key.pem
-
-kubectl -n consul create secret tls tls-server \
-  --cert deploy/tls/certs/vault.pem \
-  --key deploy/tls/certs/vault-key.pem
+##creating secrets
+#kubectl -n consul create secret tls tls-ca \
+# --cert deploy/tls/certs/ca.pem  \
+# --key deploy/tls/certs/ca-key.pem
+#
+#kubectl -n consul create secret tls tls-server \
+#  --cert deploy/tls/certs/vault.pem \
+#  --key deploy/tls/certs/vault-key.pem
